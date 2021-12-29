05070d28b2961b31aa6d1f8556de5941218cb973
hassanSRE <hassan.almitib@gmail.com>
2021-12-29 22:25:44 +1100

    Initial project add

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgdi.controllers;
+
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello World");
+
+        return "Hi Folks!";
+    }
+}
